93a256be0419be259a2a79af02f13759b0504cb2
Simone <150595274+simonetimi@users.noreply.github.com>
2024-04-13 22:06:39 +0200

    feat: add basic signup logic with connection to db

--- actions/signup.ts
+++ actions/signup.ts
@@ -1,13 +1,41 @@
 'use server';
 import * as z from 'zod';
+import { hash } from 'bcrypt';
+import { db } from '@/lib/db';
 import { SignupSchema } from '@/schemas';
 
 export const signup = async (values: z.infer<typeof SignupSchema>) => {
   const validatedFields = SignupSchema.safeParse(values);
 
-  if (!validatedFields) {
+  if (!validatedFields.success) {
     return { error: 'Invalid fields' };
   }
+
+  const { name, email, password } = validatedFields.data;
+
+  // check if user exists
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (existingUser) {
+    return { error: 'Email already in use!' };
+  }
+
+  // hash password
+  const hashedPassword = await hash(password, 12);
+
+  // create and save new user
+  const newUser = await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: send verification email (token)
 
   return { success: 'Signed up!' };
 };
