a58f5395ab8ecdd950d87ac84853a582c61dbc01
Simone <150595274+simonetimi@users.noreply.github.com>
2024-04-13 20:54:49 +0200

    feat: add prisma and configure base schema

---
+++ schemas/index.ts
@@ -0,0 +1,37 @@
+import * as z from 'zod';
+
+export const LoginSchema = z.object({
+  email: z.string().email('Email is required'),
+  password: z.string().min(1, 'Password is required'),
+});
+
+export const SignupSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(2, 'Name must be at least 2 characters')
+    .max(50, "Name can't be longer than 50 characters"),
+  email: z
+    .string()
+    .email('Email is required')
+    .toLowerCase()
+    .trim()
+    .min(4)
+    .max(254),
+  password: z
+    .string()
+    .min(6, 'Password must be at least 6 characters')
+    .max(256)
+    .refine(
+      (value) => /[A-Z]/.test(value),
+      'Password must contain at least one uppercase letter',
+    )
+    .refine(
+      (value) => /[a-z]/.test(value),
+      'Password must contain at least one lowercase letter',
+    )
+    .refine(
+      (value) => /[0-9]/.test(value),
+      'Password must contain at least one number',
+    ),
+});
