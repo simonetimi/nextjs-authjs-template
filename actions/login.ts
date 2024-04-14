b8863dd00427d2a456f4566a9393264235973979
Simone <150595274+simonetimi@users.noreply.github.com>
2024-04-14 10:44:40 +0200

    feat: implement credential signin into login server action

--- actions/login.ts
+++ actions/login.ts
@@ -1,6 +1,9 @@
 'use server';
+import { AuthError } from 'next-auth';
 import * as z from 'zod';
 
+import { signIn } from '@/auth';
+import { DEFAULT_LOGGED_IN_REDIRECT } from '@/routes';
 import { LoginSchema } from '@/schemas';
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
@@ -8,5 +11,25 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
 
   if (!validatedFields.success) {
     return { error: 'Invalid fields' };
+  }
+
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGGED_IN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return { error: 'Invalid credentials' };
+        default:
+          return { error: 'Something went wrong' };
+      }
+    }
+    throw error;
   }
 };
